8bb4c341c697b4c48ecd865f1d44e7d51bfe7e82
CARLOS JULIO BAYAS CHAVES <carlos.bayas@epn.edu.ec>
2022-02-22 11:35:51 -0500

    cambios

--- projecto_s/Form1.Designer.cs
+++ projecto_s/Form1.Designer.cs
@@ -29,25 +29,14 @@
         private void InitializeComponent()
         {
             this.linkLabel2 = new System.Windows.Forms.LinkLabel();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.lblApellido = new System.Windows.Forms.Label();
             this.lblNombre = new System.Windows.Forms.Label();
-            this.lblDestino = new System.Windows.Forms.Label();
             this.lblTitulo = new System.Windows.Forms.Label();
             this.lblCi = new System.Windows.Forms.Label();
-            this.cmbProvincias = new System.Windows.Forms.ComboBox();
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.txtApellido = new System.Windows.Forms.TextBox();
             this.txtCI = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtAsientos = new System.Windows.Forms.TextBox();
-            this.rbtSi = new System.Windows.Forms.RadioButton();
-            this.rbtNo = new System.Windows.Forms.RadioButton();
-            this.btnComprar = new System.Windows.Forms.Button();
-            this.txtNumeroMaletas = new System.Windows.Forms.TextBox();
-            this.txtInformación = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -59,17 +48,6 @@
             this.linkLabel2.Name = "linkLabel2";
             this.linkLabel2.Size = new System.Drawing.Size(0, 13);
             this.linkLabel2.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(399, 135);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 16);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Maletas";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
@@ -84,7 +62,7 @@
             // 
             this.lblApellido.AutoSize = true;
             this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblApellido.Location = new System.Drawing.Point(79, 172);
+            this.lblApellido.Location = new System.Drawing.Point(239, 185);
             this.lblApellido.Name = "lblApellido";
             this.lblApellido.Size = new System.Drawing.Size(66, 16);
             this.lblApellido.TabIndex = 8;
@@ -94,22 +72,11 @@
             // 
             this.lblNombre.AutoSize = true;
             this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombre.Location = new System.Drawing.Point(79, 131);
+            this.lblNombre.Location = new System.Drawing.Point(242, 135);
             this.lblNombre.Name = "lblNombre";
             this.lblNombre.Size = new System.Drawing.Size(63, 16);
             this.lblNombre.TabIndex = 9;
             this.lblNombre.Text = "Nombre";
-            // 
-            // lblDestino
-            // 
-            this.lblDestino.AutoSize = true;
-            this.lblDestino.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDestino.Location = new System.Drawing.Point(79, 90);
-            this.lblDestino.Name = "lblDestino";
-            this.lblDestino.Size = new System.Drawing.Size(61, 16);
-            this.lblDestino.TabIndex = 10;
-            this.lblDestino.Text = "Destino";
-            this.lblDestino.Click += new System.EventHandler(this.lblDestino_Click);
             // 
             // lblTitulo
             // 
@@ -127,35 +94,16 @@
             // 
             this.lblCi.AutoSize = true;
             this.lblCi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCi.Location = new System.Drawing.Point(86, 210);
+            this.lblCi.Location = new System.Drawing.Point(268, 236);
             this.lblCi.Name = "lblCi";
             this.lblCi.Size = new System.Drawing.Size(24, 18);
             this.lblCi.TabIndex = 12;
             this.lblCi.Text = "CI";
             this.lblCi.Click += new System.EventHandler(this.label8_Click);
             // 
-            // cmbProvincias
-            // 
-            this.cmbProvincias.FormattingEnabled = true;
-            this.cmbProvincias.Items.AddRange(new object[] {
-            "ESMERALDAS $9",
-            "MANABI $10",
-            "GUAYAQUIL $20",
-            "CUENCA $25",
-            "LOJA",
-            "AMBATO",
-            "LATACUNGA",
-            "IBARRA",
-            "RIOBAMBA",
-            "TULCAN"});
-            this.cmbProvincias.Location = new System.Drawing.Point(202, 82);
-            this.cmbProvincias.Name = "cmbProvincias";
-            this.cmbProvincias.Size = new System.Drawing.Size(121, 21);
-            this.cmbProvincias.TabIndex = 13;
-            // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(202, 128);
+            this.txtNombre.Location = new System.Drawing.Point(355, 131);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(100, 20);
             this.txtNombre.TabIndex = 14;
@@ -163,100 +111,17 @@
             // 
             // txtApellido
             // 
-            this.txtApellido.Location = new System.Drawing.Point(202, 172);
+            this.txtApellido.Location = new System.Drawing.Point(355, 181);
             this.txtApellido.Name = "txtApellido";
             this.txtApellido.Size = new System.Drawing.Size(100, 20);
             this.txtApellido.TabIndex = 15;
             // 
             // txtCI
             // 
-            this.txtCI.Location = new System.Drawing.Point(202, 210);
+            this.txtCI.Location = new System.Drawing.Point(355, 234);
             this.txtCI.Name = "txtCI";
             this.txtCI.Size = new System.Drawing.Size(100, 20);
             this.txtCI.TabIndex = 16;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(383, 85);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(148, 16);
-            this.label1.TabIndex = 17;
-            this.label1.Text = "Numero de Asientos";
-            this.label1.Click += new System.EventHandler(this.label1_Click_1);
-            // 
-            // txtAsientos
-            // 
-            this.txtAsientos.Location = new System.Drawing.Point(546, 85);
-            this.txtAsientos.Name = "txtAsientos";
-            this.txtAsientos.Size = new System.Drawing.Size(100, 20);
-            this.txtAsientos.TabIndex = 18;
-            // 
-            // rbtSi
-            // 
-            this.rbtSi.AutoSize = true;
-            this.rbtSi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbtSi.Location = new System.Drawing.Point(512, 131);
-            this.rbtSi.Name = "rbtSi";
-            this.rbtSi.Size = new System.Drawing.Size(40, 20);
-            this.rbtSi.TabIndex = 19;
-            this.rbtSi.TabStop = true;
-            this.rbtSi.Text = "Si";
-            this.rbtSi.UseVisualStyleBackColor = true;
-            // 
-            // rbtNo
-            // 
-            this.rbtNo.AutoSize = true;
-            this.rbtNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rbtNo.Location = new System.Drawing.Point(512, 155);
-            this.rbtNo.Name = "rbtNo";
-            this.rbtNo.Size = new System.Drawing.Size(46, 20);
-            this.rbtNo.TabIndex = 20;
-            this.rbtNo.TabStop = true;
-            this.rbtNo.Text = "No";
-            this.rbtNo.UseVisualStyleBackColor = true;
-            // 
-            // btnComprar
-            // 
-            this.btnComprar.BackColor = System.Drawing.Color.Maroon;
-            this.btnComprar.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnComprar.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnComprar.Location = new System.Drawing.Point(347, 251);
-            this.btnComprar.Name = "btnComprar";
-            this.btnComprar.Size = new System.Drawing.Size(91, 33);
-            this.btnComprar.TabIndex = 21;
-            this.btnComprar.Text = "Comprar";
-            this.btnComprar.UseVisualStyleBackColor = false;
-            this.btnComprar.Click += new System.EventHandler(this.btnComprar_Click);
-            // 
-            // txtNumeroMaletas
-            // 
-            this.txtNumeroMaletas.Location = new System.Drawing.Point(546, 194);
-            this.txtNumeroMaletas.Name = "txtNumeroMaletas";
-            this.txtNumeroMaletas.Size = new System.Drawing.Size(100, 20);
-            this.txtNumeroMaletas.TabIndex = 22;
-            // 
-            // txtInformación
-            // 
-            this.txtInformación.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.txtInformación.Location = new System.Drawing.Point(224, 309);
-            this.txtInformación.Multiline = true;
-            this.txtInformación.Name = "txtInformación";
-            this.txtInformación.Size = new System.Drawing.Size(338, 125);
-            this.txtInformación.TabIndex = 23;
-            this.txtInformación.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(367, 198);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(143, 16);
-            this.label4.TabIndex = 24;
-            this.label4.Text = "Número de maletas";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // pictureBox1
             // 
@@ -276,25 +141,14 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(803, 460);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.txtInformación);
-            this.Controls.Add(this.txtNumeroMaletas);
-            this.Controls.Add(this.btnComprar);
-            this.Controls.Add(this.rbtNo);
-            this.Controls.Add(this.rbtSi);
-            this.Controls.Add(this.txtAsientos);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtCI);
             this.Controls.Add(this.txtApellido);
             this.Controls.Add(this.txtNombre);
-            this.Controls.Add(this.cmbProvincias);
             this.Controls.Add(this.lblCi);
             this.Controls.Add(this.lblTitulo);
-            this.Controls.Add(this.lblDestino);
             this.Controls.Add(this.lblNombre);
             this.Controls.Add(this.lblApellido);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.linkLabel2);
             this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.DoubleBuffered = true;
@@ -311,25 +165,14 @@
         #endregion
 
         private System.Windows.Forms.LinkLabel linkLabel2;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lblApellido;
         private System.Windows.Forms.Label lblNombre;
-        private System.Windows.Forms.Label lblDestino;
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.Label lblCi;
-        private System.Windows.Forms.ComboBox cmbProvincias;
         private System.Windows.Forms.TextBox txtNombre;
         private System.Windows.Forms.TextBox txtApellido;
         private System.Windows.Forms.TextBox txtCI;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtAsientos;
-        private System.Windows.Forms.RadioButton rbtSi;
-        private System.Windows.Forms.RadioButton rbtNo;
-        private System.Windows.Forms.Button btnComprar;
-        private System.Windows.Forms.TextBox txtNumeroMaletas;
-        private System.Windows.Forms.TextBox txtInformación;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
